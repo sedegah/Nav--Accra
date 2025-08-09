9bcc74b8e93de3ea111ca2dcf2b67267929bf187
Kimathi Sedegah <sedkim370@gmail.com>
2025-08-09 20:18:36 +0000

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -1,44 +1,44 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import qualified Data.Map.Strict as Map
 import qualified Data.Set as Set
-import Data.Maybe (fromMaybe, isJust)
+import Data.Maybe (fromMaybe)
 import Data.List (minimumBy, intercalate)
 import Data.Ord (comparing)
-import Control.Monad (when)
 
 type Node = String
 type Weight = Double
 type Graph = Map.Map Node [(Node, Weight)]
 
--- Sample Accra locations graph (25 nodes)
 graph :: Graph
 graph = Map.fromList
   [ ("Kotoka Airport", [("Osu",7.0), ("Achimota",15.0), ("Madina",20.0)])
-  , ("Osu", [("Kotoka Airport",7.0), ("Accra Central",4.0), ("Labadi",3.5)])
-  , ("Achimota", [("Kotoka Airport",15.0), ("Accra Central",10.0), ("Mallam",5.0)])
-  , ("Accra Central", [("Osu",4.0), ("Achimota",10.0), ("Labadi",5.0), ("Circle",6.0)])
+  , ("Osu", [("Kotoka Airport",7.0), ("Accra Central",4.0), ("Labadi",3.5), ("Adabraka",6.0)])
+  , ("Achimota", [("Kotoka Airport",15.0), ("Accra Central",10.0), ("Mallam",5.0), ("Achimota Mall",3.0)])
+  , ("Accra Central", [("Osu",4.0), ("Achimota",10.0), ("Labadi",5.0), ("Circle",6.0), ("Adabraka",5.0), ("East Legon",12.0)])
   , ("Labadi", [("Osu",3.5), ("Accra Central",5.0), ("Teshie",7.0)])
-  , ("Madina", [("Kotoka Airport",20.0), ("Tema",25.0), ("Adenta",8.0)])
-  , ("Mallam", [("Achimota",5.0), ("Tudu",12.0)])
-  , ("Circle", [("Accra Central",6.0), ("Tudu",3.0), ("Tema",28.0)])
-  , ("Tudu", [("Mallam",12.0), ("Circle",3.0), ("Nungua",18.0)])
-  , ("Tema", [("Madina",25.0), ("Circle",28.0), ("Nungua",15.0)])
+  , ("Madina", [("Kotoka Airport",20.0), ("Tema",25.0), ("Adenta",8.0), ("East Legon",10.0), ("University of Ghana",4.0), ("Kwabenya",10.0)])
+  , ("Mallam", [("Achimota",5.0), ("Tudu",12.0), ("Kaneshie",8.0)])
+  , ("Circle", [("Accra Central",6.0), ("Tudu",3.0), ("Tema",28.0), ("Adabraka",4.0)])
+  , ("Tudu", [("Mallam",12.0), ("Circle",3.0), ("Nungua",18.0), ("Kaneshie",5.0)])
+  , ("Tema", [("Madina",25.0), ("Circle",28.0), ("Nungua",15.0), ("Spintex",15.0)])
   , ("Nungua", [("Tema",15.0), ("Tudu",18.0), ("Teshie",5.0)])
   , ("Teshie", [("Labadi",7.0), ("Nungua",5.0), ("Ada",40.0)])
   , ("Adenta", [("Madina",8.0), ("Pantang",6.0)])
   , ("Pantang", [("Adenta",6.0), ("Abokobi",7.0)])
   , ("Abokobi", [("Pantang",7.0), ("Ofankor",10.0)])
   , ("Ofankor", [("Abokobi",10.0), ("Spintex",12.0)])
-  , ("Spintex", [("Ofankor",12.0), ("Tema",15.0)])
-  , ("East Legon", [("Madina",10.0), ("Spintex",14.0)])
+  , ("Spintex", [("Ofankor",12.0), ("Tema",15.0), ("East Legon",14.0)])
+  , ("East Legon", [("Madina",10.0), ("Spintex",14.0), ("University of Ghana",5.0), ("Kwabenya",8.0), ("Accra Central",12.0)])
   , ("University of Ghana", [("East Legon",5.0), ("Madina",4.0)])
   , ("Kwabenya", [("East Legon",8.0), ("Madina",10.0)])
-  , ("Adabraka", [("Accra Central",5.0), ("Osu",6.0)])
+  , ("Adabraka", [("Accra Central",5.0), ("Osu",6.0), ("Circle",4.0)])
   , ("Circle", [("Adabraka",4.0), ("Accra Central",6.0), ("Tudu",3.0)])
   , ("Kaneshie", [("Mallam",8.0), ("Tudu",5.0)])
-  , ("Tudu", [("Kaneshie",5.0), ("Mallam",12.0), ("Circle",3.0)])
   , ("Achimota Mall", [("Achimota",3.0), ("Mallam",7.0)])
+  , ("Ada", [("Teshie",40.0)])
   ]
 
 -- Dijkstra's algorithm with path reconstruction
@@ -61,7 +61,7 @@ dijkstra graph start = go initialDist initialPrev initialQueue
           in go dist' prev' queue'
 
     relaxNeighbors :: Node -> [(Node, Weight)] -> Map.Map Node Weight -> Map.Map Node (Maybe Node) -> (Map.Map Node Weight, Map.Map Node (Maybe Node))
-    relaxNeighbors u [] dist prev = (dist, prev)
+    relaxNeighbors _ [] dist prev = (dist, prev)
     relaxNeighbors u ((v,w):ns) dist prev =
       let alt = Map.findWithDefault (1/0) u dist + w
           oldDist = Map.findWithDefault (1/0) v dist
